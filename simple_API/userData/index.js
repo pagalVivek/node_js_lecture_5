ef3bb377171b83804108f60380946f380dba0878
unknown <naruto567uzumaki009@gmail.com>
2023-12-30 16:03:51 +0530

    !

--- simple_API/userData/index.js
+++ simple_API/userData/index.js
@@ -1,1 +1,29 @@
 const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer( (req, res) => {
+    if (req.url == "/" || req.url == "/home"){
+        res.end("Hello from Home site!")
+    }
+    else if( req.url == "/" || req.url == "/about"){
+        console.log("hello from about")
+    } 
+    else if( req.url == "/" || req.url == "/contact"){
+        console.log("hello from contact")
+    }
+    else if( req.url == "/" || req.url == "/userapi"){
+        fs.readFile("C:/Users/Admin/Desktop/npmNode/simple_API/userAPI/userapi.json", "utf-8", (err, data) => {
+            console.log(err)
+            const objData = JSON.parse(data)
+            res.end(objData[0].price)
+            
+        })
+    } 
+    else{
+        console.log("ERROR 404 : Page not found ")
+    }
+})
+server.listen(8000, '127.0.0.1', () => {
+    console.log("server started!")
+})
+
